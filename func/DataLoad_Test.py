6b2c02ebee06e14d3de866dc1f11161620b631c3
YangFangShu <yfs2016@hit.edu.cn>
2021-06-03 21:36:47 +0800

    correct the space

--- func/DataLoad_Test.py
+++ func/DataLoad_Test.py
@@ -30,19 +30,19 @@ def DataLoad_Test(test_size,test_data_dir,data_dim,in_channels,model_dim,data_ds
                 test1_set  = data11_set
             else:
                 test1_set  = np.append(test1_set,data11_set,axis=0)
-            filename_label = test_data_dir+'vmodel_test/'+truthfilename+str(i)
-            data2_set      = scipy.io.loadmat(filename_label)
-            data2_set      = np.float32(data2_set[str(truthname)].reshape(model_dim))
-            data2_set      = block_reduce(data2_set,block_size=label_dsp_blk,func=np.max)
-            label_dsp_dim  = data2_set.shape
-            data2_set      = data2_set.reshape(1,label_dsp_dim[0]*label_dsp_dim[1])
-            data2_set      = np.float32(data2_set)
-            if i==start:
-                test_set   = test1_set
-                label_set  = data2_set
-            else:
-                test_set   = np.append(test_set,test1_set,axis=0)
-                label_set  = np.append(label_set,data2_set,axis=0)
+        filename_label = test_data_dir+'vmodel_test/'+truthfilename+str(i)
+        data2_set      = scipy.io.loadmat(filename_label)
+        data2_set      = np.float32(data2_set[str(truthname)].reshape(model_dim))
+        data2_set      = block_reduce(data2_set,block_size=label_dsp_blk,func=np.max)
+        label_dsp_dim  = data2_set.shape
+        data2_set      = data2_set.reshape(1,label_dsp_dim[0]*label_dsp_dim[1])
+        data2_set      = np.float32(data2_set)
+        if i==start:
+            test_set   = test1_set
+            label_set  = data2_set
+        else:
+            test_set   = np.append(test_set,test1_set,axis=0)
+            label_set  = np.append(label_set,data2_set,axis=0)
 
     test_set  = test_set.reshape((test_size,in_channels,data_dsp_dim[0]*data_dsp_dim[1]))
     label_set = label_set.reshape((test_size,label_dsp_dim[0]*label_dsp_dim[1]))
